fff8db2e8eb796d6499faefe8c74f2d0baf5828d
Schrese <53787633+Schrese@users.noreply.github.com>
2020-01-06 16:37:31 -0500

    Made Login/Signup pages Not complete yet Starting to think about Redux

--- chef-port/src/components/LogIn.js
+++ chef-port/src/components/LogIn.js
@@ -1,16 +1,50 @@
 import React, {useState} from 'react';
-
+import {Button, Form} from 'semantic-ui-react';
 //utils
 import {axiosWithAuth} from '../utils/axiosWithAuth';
 
+const initState = {
+    username: '',
+    password: ''
+}
+
 const LogIn = () => {
+    const [user, setUser] = useState(initState);
+
+    const changeHandler = e => {
+        e.preventDefault();
+        let value = e.target.value
+        setUser({
+            ...user,
+            [e.target.name]: value
+        })
+    }
+
+    const submit = e => {
+        e.preventDefault();
+        axiosWithAuth()
+            .post('/user/login', user)
+            .then(res => {
+                console.log('from login submit', res)
+            })
+            .catch(err => {
+                console.log('something wrong in submit')
+            })
+    }
 
     return(
         <div>
-            <form>
-                <input type = 'text' placeholder = 'Username' name = 'username' />
-                <input type = 'text' placeholder = 'Password' name = 'password' />
-            </form>
+            <Form onSubmit = {submit}>
+                <Form.Field>
+                    <label>Username</label>
+                    <input type = 'text' placeholder = 'Username' onChange = {changeHandler} name = 'username' />
+                </Form.Field>
+                <Form.Field>
+                    <label>Password</label>
+                    <input type = 'text' placeholder = 'Password' onChange = {changeHandler} name = 'password' />
+                </Form.Field>
+                <Button type = 'submit'>Log In</Button>
+            </Form>
         </div>
     )
 }
