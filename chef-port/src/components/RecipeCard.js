6e245dd5132cfa36d4b70b0898ef3152b1013eeb
Daniel Ferrer <danfrrr13@gmail.com>
2020-01-07 13:55:12 -0500

    Worked on RecipeCard component

--- chef-port/src/components/RecipeCard.js
+++ chef-port/src/components/RecipeCard.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+
+import NavBar from './NavBar';
 
 import {getOneRecipe} from '../actions';
 
 const RecipeCard = props => {
 
+    useEffect(() => {
+        props.getOneRecipe();
+    }, [props.getOneRecipe]);
+
     return(
-        <div>
-            <h2>One Recipe, One Life!</h2>
+        <div class="recipeCard">
+            <NavBar />
+            <div className="recipe">
+                <h2>Recipe Card</h2>
+                <Card key = {props.recipe.id} onClick = {getOneRecipe}>
+                    <Image src = {props.recipe.image_url} />
+                    <Card.Content>
+                        <Card.Header>{props.recipe.name}</Card.Header>
+                        <Card.Description>{props.recipe.chef}</Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                        <p>{props.recipe.description}</p>
+                    </Card.Content>
+                </Card>
+            </div>
         </div>
     )
 }
 
+const mapStateToProps = state => {
+    return {
+        recipe: state.recipe
+    }
+}
 
-export default RecipeCard;
+export default connect(mapStateToProps, {getOneRecipes})(RecipeCard);
