b2eac9ac876622654d6d0c27cf940f1d1d5b000b
Schrese <53787633+Schrese@users.noreply.github.com>
2020-01-06 19:56:30 -0500

    Got home page mostly populated But images aren't working right now

--- chef-port/src/actions/userActions.js
+++ chef-port/src/actions/userActions.js
@@ -10,13 +10,17 @@ export const SIGNUP_START = 'SIGNUP_START';
 export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
 export const SIGNUP_FAILURE = 'SIGNUP_FAILURE';
 
+//fetching all recipes for home page
+export const FETCH_ALL_START = 'FETCH_ALL_START';
+export const FETCH_ALL_SUCCESS = 'FETCH_ALL_SUCCESS';
+export const FETCH_ALL_FAILURE = 'FETCH_ALL_FAILURE';
 
 export const login = (userInfo, history) => dispatch => {
     dispatch({type: LOGIN_START})
     axiosWithAuth()
         .post('/user/login', userInfo)
         .then(res => {
-            console.log('res from login action', res)
+            // console.log('res from login action', res)
             localStorage.setItem('token', res.data.token)
             dispatch({type: LOGIN_SUCCESS, payload: res.data})
             history.push('/dashboard')
@@ -29,10 +33,24 @@ export const signup = (userInfo, history) => dispatch => {
     axiosWithAuth()
         .post('/user/register', userInfo)
         .then(res => {
-            console.log('res from signup action', res)
+            // console.log('res from signup action', res)
             localStorage.setItem('token', res.data.token)
             dispatch({type: SIGNUP_SUCCESS, payload: res.data})
             history.push('/dashboard')
         })
         .catch((err) => dispatch({type: SIGNUP_FAILURE}))
+}
+
+export const getAllRecipes = () => dispatch => {
+    dispatch({type: FETCH_ALL_START})
+    axiosWithAuth()
+        .get('/user/recipes')
+        .then(res => {
+            console.log('res from get all recipes', res)
+            dispatch({type: FETCH_ALL_SUCCESS, payload: res.data})
+        })
+        .catch(err => {
+            console.log('err from get all recipes', err)
+            dispatch({type: FETCH_ALL_FAILURE, payload: err.res})
+        })
 }
--- chef-port/src/reducers/userReducer.js
+++ chef-port/src/reducers/userReducer.js
@@ -4,14 +4,18 @@ import {
     LOGIN_FAILURE,
     SIGNUP_START,
     SIGNUP_SUCCESS,
-    SIGNUP_FAILURE
+    SIGNUP_FAILURE,
+    FETCH_ALL_START,
+    FETCH_ALL_SUCCESS,
+    FETCH_ALL_FAILURE
 
 } from "../actions";
 
 const initState = {
     error: null,
     isFetching: false,
-    currentUser: {}
+    currentUser: {},
+    recipes: []
 }
 
 export const reducer = (state = initState, {type , payload}) => {
@@ -37,7 +41,46 @@ export const reducer = (state = initState, {type , payload}) => {
                 isFetching: true,
                 currentUser: ''
             }
-
+        case LOGIN_START: 
+            return {
+                ...state,
+                isFetching: true,
+                error: null,
+                currentUser: ''
+            }
+        case LOGIN_SUCCESS: 
+            return {
+                ...state,
+                error: null,
+                isFetching: false,
+                currentUser: payload
+            }
+        case LOGIN_FAILURE: 
+            return {
+                ...state,
+                error: "Wrong Username or Password", 
+                isFetching: false,
+                currentUser: ''
+            }
+        case FETCH_ALL_START: 
+            return {
+                ...state, 
+                isFetching: true,
+                error: null,
+            }
+        case FETCH_ALL_SUCCESS: 
+            return {
+                ...state, 
+                recipes: payload,
+                isFetching: false,
+                error: ''
+            }
+        case FETCH_ALL_FAILURE: 
+            return {
+                ...state,
+                isFetching: false,
+                error: payload
+            }
         default: 
             return state;
     }
