a2233744bceb55d4fdce24238334fbd7fe49ba5a
Schrese <53787633+Schrese@users.noreply.github.com>
2020-01-08 13:47:03 -0500

    Got edit functional
    Co-authored-by: Daniel Ferrer <danfrrr13@gmail.com>

--- chef-port/src/components/EditRecipe.js
+++ chef-port/src/components/EditRecipe.js
@@ -7,19 +7,22 @@ import NavBar from './NavBar';
 import {editRecipe} from '../actions';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const initState = {
-    name:  '',
-    description: '',
-    image_url: '',
-    meal_type: '',
-    ingredients: '',
-    instructions: ''
-}
+
  
 const EditRecipe = (props) => {
     console.log(props)
-    const [formValues, setFormValues] = useState({...initState})
 
+    const initState = {
+        name:  props.recipe.name,
+        description: props.recipe.description,
+        image_url: props.recipe.image_url,
+        meal_type: props.recipe.meal_type,
+        ingredients: props.recipe.ingredients,
+        instructions: props.recipe.instructions
+    }
+
+    const [formValues, setFormValues] = useState(initState)
+    
     const changeHandler = e => {
         e.preventDefault();
         let value = e.target.value;
@@ -28,8 +31,10 @@ const EditRecipe = (props) => {
 
     const submitHandler = e => {
         e.preventDefault();
-        editRecipe(formValues, props.history);
+        props.editRecipe(formValues, props.recipe.id, props.history);
     }
+
+    console.log(formValues)
 
     return(
         <div>
@@ -37,27 +42,27 @@ const EditRecipe = (props) => {
             <Form onSubmit = {submitHandler}>
                 <Form.Field>
                     <label>Name:</label>
-                    <input type = 'text' placeholder = 'name' name = 'name' onChange = {changeHandler}  />
+                    <input type = 'text' placeholder = 'name' name = 'name' onChange = {changeHandler} value = {formValues.name} />
                 </Form.Field>
                 <Form.Field>
                     <label>Description:</label>
-                    <input type = 'text' placeholder = 'description' name = 'description' onChange = {changeHandler} />
+                    <input type = 'text' placeholder = 'description' name = 'description' onChange = {changeHandler} value = {formValues.description}  />
                 </Form.Field>
                 <Form.Field>
                     <label>Image:</label>
-                    <input type = 'text' placeholder = 'image url' name = 'image_url' onChange = {changeHandler} />
+                    <input type = 'text' placeholder = 'image url' name = 'image_url' onChange = {changeHandler} value = {formValues.image_url}  />
                 </Form.Field>
                 <Form.Field>
                     <label>Meal Type:</label>
-                    <input type = 'text' placeholder = 'meal type' name = 'meal_type' onChange = {changeHandler} />
+                    <input type = 'text' placeholder = 'meal type' name = 'meal_type' onChange = {changeHandler} value = {formValues.meal_type}  />
                 </Form.Field>
                 <Form.Field>
                     <label>Ingredients:</label>
-                    <input type = 'text' placeholder = 'ingredients' name = 'ingredients' onChange = {changeHandler} />
+                    <input type = 'text' placeholder = 'ingredients' name = 'ingredients' onChange = {changeHandler} value = {formValues.ingredients}  />
                 </Form.Field>
                 <Form.Field>
                     <label>Instructions:</label>
-                    <input type = 'text' placeholder = 'instructions' name = 'instructions' onChange = {changeHandler} />
+                    <input type = 'text' placeholder = 'instructions' name = 'instructions' onChange = {changeHandler} value = {formValues.instructions} />
                 </Form.Field>
                 <Button type = 'submit'>Submit</Button>
             </Form>
